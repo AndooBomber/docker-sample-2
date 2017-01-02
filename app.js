2e52d9d1e70a34eb3aa86ebbe9576bdb1a63cca8
Keita Moromizato <keita_moromizato@relationsgroup.co.jp>
2017-01-02 17:23:18 +0900

    compose setting

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const data = [
+  { text: 'hoge' },
+  { text: 'huga' }
+];
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(data));
+
+}).listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
